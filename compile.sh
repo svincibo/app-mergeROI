ba2f690e986a6663a581311e2de026b746016888
Sophia Vinci-Booher <svincibo@gmail.com>
2020-04-20 15:22:51 -0400

    recompiled with logs

--- compile.sh
+++ compile.sh
@@ -3,6 +3,14 @@ module load matlab/2019a
 
 mkdir -p compiled
 
+log=compiled/commit_ids.txt
+true > $log
+echo "/N/u/brlife/git/jsonlab" >> $log
+(cd /N/u/brlife/git/jsonlab && git log -1) >> $log
+
+echo "/N/u/brlife/git/vistasoft" >> $log
+(cd /N/u/brlife/git/vistasoft && git log -1) >> $log
+
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
